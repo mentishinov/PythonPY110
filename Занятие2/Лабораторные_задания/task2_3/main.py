cc95927320920d05fe0000664b40be32311e9c4e
alemen <t@.ru>
2021-11-16 20:58:12 +0300

    практика 16.11

--- Занятие2/Лабораторные_задания/task2_3/main.py
+++ Занятие2/Лабораторные_задания/task2_3/main.py
@@ -1,5 +1,8 @@
 def pow_gen(base: int):
-    ...  # TODO записать функцию-генератор
+    n = 0
+    while True:
+        yield base ** n
+        n += 1
 
 
 if __name__ == "__main__":
@@ -7,3 +10,16 @@ if __name__ == "__main__":
 
     for _ in range(5):
         print(next(pow_numbers))
+
+
+
+# def count(start_number: float = 1, step: float = 1):
+#     while True:
+#         yield start_number
+#         start_number += step
+#
+#
+# if __name__ == "__main__":
+#     my_count = count(10, 0.5)
+#     for _ in range(10):
+#         print(next(my_count))
